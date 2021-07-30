5f2fd2fa407cfb67d4565aae9a516b7d93b434f3
tester <test1111111test@yandex.ru>
2021-07-30 08:24:41 +0300

    ready-made homework1

--- test_items.py
+++ test_items.py
@@ -5,6 +5,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_present_page(browser):
     browser.get(link)
+    #time.sleep(7)
     button = browser.find_element_by_css_selector("#add_to_basket_form")
-    time.sleep(7)
     assert button, "No such button on the page"
